e8500a471e8af9ba919c1c65f7a04d6171eb1c2a
syed abdul qahar <syedabdulqahar2004@gmail.com>
2026-03-16 09:38:00 +0500

    Asynchronous :  Callbacks

---
+++ JavaScript Fundamentals/Async/Callbacks-1.js
@@ -0,0 +1,20 @@
+/*Tumhe ek school system simulate karna hai:
+Ek function getStudent(id, callback) hai, jo student details return karta hai.
+Ek function getMarks(student, callback) hai, jo student ke marks return karta hai.
+Tumhe console mein print karna hai:
+Student Name: <name>
+Marks: <marks>
+Hints:
+Dono functions asynchronous behavior ko simulate karne ke liye setTimeout use karenge.
+Callback ka use karna mandatory hai.*/
+const Student = [
+  { id: 1, name: "Danish" },
+  { id: 2, name: "Ali" },
+];
+const marks = [
+  { id: 1, marks: 50 },
+  { id: 2, marks: 60 },
+];
+function getStudent(id, callback) {
+  setTimeout(() => {});
+}
